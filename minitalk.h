33d2d7358f78d8ce970712b8d1c6b293c91207b7
Asaad Naji <asnaji@e2r1p1.1337.ma>
2023-12-12 22:36:12 +0100

    m

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: asnaji <asnaji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:39:59 by asnaji            #+#    #+#             */
-/*   Updated: 2023/12/12 22:16:00 by asnaji           ###   ########.fr       */
+/*   Updated: 2023/12/12 22:35:28 by asnaji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,11 @@
 # define MINITALK_H
 # include <signal.h>
 # include <unistd.h>
-# include <stdio.h>
-# include <limits.h>
-# include <stdio.h>
 # include "ft_printf/ft_printf.h"
 
 int				ft_atoi(char *str);
 void			print_bits(unsigned char octet);
 unsigned char	reverse_bits(unsigned char octet);
 void			send_bit(pid_t targetPID, int bit, int *index);
-unsigned char	reverse_bits(unsigned char octet);
 
 #endif
